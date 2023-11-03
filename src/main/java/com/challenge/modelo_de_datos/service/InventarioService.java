8ef24353fb46262ecffb8a7ce62944512605ed40
Jair Velazquez <jairvelaz54.celaya@gmail.com>
2023-11-03 16:41:53 -0600

    agregando validaciones en el archivo InventarioService

--- src/main/java/com/challenge/modelo_de_datos/service/InventarioService.java
+++ src/main/java/com/challenge/modelo_de_datos/service/InventarioService.java
@@ -29,15 +29,41 @@ public class InventarioService {
     public ResponseEntity<Object> newInventario(Inventario inventario) {
         HashMap<String, Object> datos = new HashMap<>();
         boolean existeProducto = this.productoRepository.existsById(inventario.getProducto().getIdProducto());
+        int id= inventario.getIdInventario();
+        if(id!=0){
+            datos.put("error",true);
+            datos.put("message", "No mandar ID, este se genera automaticamente");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+        if (inventario.getEntrada()<=0){
+            return createErrorResponse("La entrada del inventario debe ser mayor que cero.");
+        }
+        if (inventario.getStockInicial()<=0){
+            return createErrorResponse("El stock inicial del inventario debe ser mayor que cero.");
+
+        }
+        if (inventario.getStockMinimo()<=0){
+            return createErrorResponse("el stock minimo del inventario debe ser mayor que cero.");
+
+        }
         if (existeProducto) {
             datos.put("message", "Se guardó con éxito");
             inventarioRepository.save(inventario);
             datos.put("data", inventario);
-            return new ResponseEntity<>(datos, HttpStatus.CREATED);
         } else {
             datos.put("message", "El producto no existe");
-            return new ResponseEntity<>(datos, HttpStatus.CREATED);
         }
+        return new ResponseEntity<>(datos, HttpStatus.CREATED);
+    }
+
+    private ResponseEntity<Object> createErrorResponse(String s) {
+        HashMap<String, Object> datos = new HashMap<>();
+        datos.put("message", s);
+        return new ResponseEntity<>(datos, HttpStatus.BAD_REQUEST);
+
     }
 
     public ResponseEntity<Object> updateInventario(Integer id, Inventario inventario) {
